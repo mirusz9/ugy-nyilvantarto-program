ef8b6e9621bbaa7b8e048ad29388c8f4c5955b0b
mirusz9 <adam.sztano@gmail.com>
2020-11-20 20:38:37 +0100

    add meeting button works now

--- src/components/AddCase/AddMeetings.jsx
+++ src/components/AddCase/AddMeetings.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormSection from './FormSection';
 import MeetingInput from './MeetingInput';
 import { AiOutlinePlus } from 'react-icons/ai';
 import styles from './AddMeetings.module.css';
 
 const AddMeetings = () => {
+	const [state, setState] = useState({ numOfMeetings: 3 });
+
+	const handleAddButtonClick = () => {
+		setState((prev) => {
+			return { numOfMeetings: prev.numOfMeetings + 1 };
+		});
+	};
+
+	const handleDeleteClick = (key) => {
+		console.log(key);
+	}
+
 	return (
 		<FormSection label="Tárgyalások">
-			<MeetingInput />
-			<MeetingInput />
-			<MeetingInput />
-			<button className={styles.AddMeetingBtn} type="button">
+			{Array.from({ length: state.numOfMeetings }).map((_, i) => {
+				return <MeetingInput key={i} handleDeleteClick={handleDeleteClick}/>;
+			})}
+			<button
+				className={styles.AddMeetingBtn}
+				type="button"
+				onClick={handleAddButtonClick}
+			>
 				<AiOutlinePlus size="2.5rem" className={styles.plusIcon} />
 			</button>
 		</FormSection>
--- src/components/AddCase/MeetingInput.jsx
+++ src/components/AddCase/MeetingInput.jsx
@@ -4,17 +4,21 @@ import Input from './Input';
 import { BsFillTrashFill } from 'react-icons/bs';
 import styles from './MeetingInput.module.css';
 
-const MeetingInput = ({ id }) => {
+const MeetingInput = ({ key, handleDeleteClick }) => {
 	return (
 		<div className={styles.meetingContainer}>
-			<Input type="text" name={`${id}-meeting-name`}>
+			<Input type="text" name={`${key}-meeting-name`}>
 				Tárgyalás Neve
 			</Input>
-			<Input type="datetime-local" name={`${id}-meeting-time`}>
+			<Input type="datetime-local" name={`${key}-meeting-time`}>
 				Tárgyalás Időpontja
 			</Input>
-			<button type="button" className={styles.trashBtn}>
-				<BsFillTrashFill className={styles.trashIcon} size="1.5rem"/>
+			<button
+				type="button"
+				className={styles.trashBtn}
+				onClick={() => handleDeleteClick(key)}
+			>
+				<BsFillTrashFill className={styles.trashIcon} size="1.5rem" />
 			</button>
 		</div>
 	);
